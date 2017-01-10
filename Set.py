a9f86d75e6c28896749087b1951df1906b3f5d9a
Marcos Mussio <marcos.mussio@autodesk.com>
2017-01-10 00:50:06 -0300

    (v 1.0.0.3) removed main section for Set.py

--- Set.py
+++ Set.py
@@ -39,17 +39,5 @@ class Set(object):
     def __str__(self):
         return " ".join(str(self.elements))
 
-if __name__ == "__main__":
-    newSet = Set([1,2,3,4,1,3,7])
-    print newSet
-    newSet.add(8)
-    newSet.add(7)
-    print newSet
-    newSetC = Set([])
-    newSetB = Set([1,2,3,4,1, 8,3,7])
-    print newSet.diff(newSetB)
-    print newSet.diffSim(newSetB)
-    print newSet.intersection(newSetB)
-    print newSetC.isIncluded(newSetB)
-    print newSet.prodCart(newSetB)
-    print len(newSetB.pot())
+
+
